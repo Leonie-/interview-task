49872f0b061e88d16a926a4c2f8ff3eafe13de08
Leonie Kenyon <lkenyon@roku.com>
2023-06-22 14:19:53 +0100

    Updates instructions

--- README.md
+++ README.md
@@ -19,5 +19,7 @@ Please have a look at the file in `src/data.js`. You will need to use this data 
 
 A rough mock-up of what this might look like is here:
 
+![](./mock-up.png)
+
 Please ensure that you update the Jest and Cypress tests.
 If you have not used React testing Library before this [Cheatsheet](https://testing-library.com/docs/react-testing-library/cheatsheet/) might be helpful.
--- src/app.js
+++ src/app.js
@@ -7,21 +7,81 @@ const App = ({ data }) => {
     return (
         <div className="interview">
             <h1>Fruit Selector</h1>
-            <label htmlFor="fruit">Choose a fruit: </label>
-            <select name="fruit" id="fruit" data-testid="fruit-select">
-                <option value="one" data-testid="fruit-option">
-                    One
-                </option>
-                <option value="two" data-testid="fruit-option">
-                    Two
-                </option>
-                <option value="three" data-testid="fruit-option">
-                    Three
-                </option>
-                <option value="four" data-testid="fruit-option">
-                    Four
-                </option>
-            </select>
+            <div className="fruit">
+                <label htmlFor="fruit">Choose a fruit: </label>
+                <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
+                    <option value="one" data-testid="fruit-option">
+                        Apples
+                    </option>
+                    <option value="two" data-testid="fruit-option">
+                        Two
+                    </option>
+                    <option value="three" data-testid="fruit-option">
+                        Three
+                    </option>
+                    <option value="four" data-testid="fruit-option">
+                        Four
+                    </option>
+                </select>
+                &nbsp;&nbsp;
+                <label htmlFor="fruit">Choose a variety: </label>
+                <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
+                    <option value="one" data-testid="fruit-option">
+                        Fuji
+                    </option>
+                    <option value="two" data-testid="fruit-option">
+                        Two
+                    </option>
+                    <option value="three" data-testid="fruit-option">
+                        Three
+                    </option>
+                    <option value="four" data-testid="fruit-option">
+                        Four
+                    </option>
+                </select>
+                <a href="" className="remove">
+                    Remove fruit
+                </a>
+            </div>
+            <div className="fruit">
+                <label htmlFor="fruit">Choose a fruit: </label>
+                <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
+                    <option value="one" data-testid="fruit-option">
+                        Bananas
+                    </option>
+                    <option value="two" data-testid="fruit-option">
+                        Two
+                    </option>
+                    <option value="three" data-testid="fruit-option">
+                        Three
+                    </option>
+                    <option value="four" data-testid="fruit-option">
+                        Four
+                    </option>
+                </select>
+                &nbsp;&nbsp;
+                <label htmlFor="fruit">Choose a variety: </label>
+                <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
+                    <option value="one" data-testid="fruit-option">
+                        Cavendish
+                    </option>
+                    <option value="two" data-testid="fruit-option">
+                        Two
+                    </option>
+                    <option value="three" data-testid="fruit-option">
+                        Three
+                    </option>
+                    <option value="four" data-testid="fruit-option">
+                        Four
+                    </option>
+                </select>
+                <a href="" className="remove">
+                    Remove fruit
+                </a>
+            </div>
+            <div>
+                <button className="add">Add a fruit</button>
+            </div>
         </div>
     );
 };
