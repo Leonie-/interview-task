88c4eac6cfacc9215e005d5a7d15a8232de3c442
Leonie Kenyon <lkenyon@roku.com>
2023-06-22 13:13:49 +0100

    Fruit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const App = () => {
         <div class="interview">
             <h1>Fruit Selector</h1>
             <label for="fruit">Choose a fruit: </label>
-            <select name="fruit" id="fruit" data-testid="metrics">
+            <select name="fruit" id="fruit" data-testid="fruit">
                 <option value="one">One</option>
                 <option value="two">Two</option>
                 <option value="three">Three</option>
