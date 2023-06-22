9f5256818764c4df7a2149898633e10ee487def9
Leonie Kenyon <lkenyon@roku.com>
2023-06-22 14:22:41 +0100

    Updates app

--- src/app.js
+++ src/app.js
@@ -10,60 +10,27 @@ const App = ({ data }) => {
             <div className="fruit">
                 <label htmlFor="fruit">Choose a fruit: </label>
                 <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
-                    <option value="one" data-testid="fruit-option">
+                    <option value="apples" data-testid="fruit-option">
                         Apples
                     </option>
-                    <option value="two" data-testid="fruit-option">
-                        Two
-                    </option>
-                    <option value="three" data-testid="fruit-option">
-                        Three
-                    </option>
-                    <option value="four" data-testid="fruit-option">
-                        Four
-                    </option>
-                </select>
-                &nbsp;&nbsp;
-                <label htmlFor="fruit">Choose a variety: </label>
-                <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
-                    <option value="one" data-testid="fruit-option">
-                        Fuji
-                    </option>
-                    <option value="two" data-testid="fruit-option">
-                        Two
-                    </option>
-                    <option value="three" data-testid="fruit-option">
-                        Three
-                    </option>
-                    <option value="four" data-testid="fruit-option">
-                        Four
-                    </option>
-                </select>
-                <a href="" className="remove">
-                    Remove fruit
-                </a>
-            </div>
-            <div className="fruit">
-                <label htmlFor="fruit">Choose a fruit: </label>
-                <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
-                    <option value="one" data-testid="fruit-option">
+                    <option value="bananas" data-testid="fruit-option">
                         Bananas
                     </option>
-                    <option value="two" data-testid="fruit-option">
-                        Two
+                    <option value="pears" data-testid="fruit-option">
+                        Pears
                     </option>
-                    <option value="three" data-testid="fruit-option">
-                        Three
+                    <option value="pineapples" data-testid="fruit-option">
+                        Pineapples
                     </option>
-                    <option value="four" data-testid="fruit-option">
-                        Four
+                    <option value="strawberries" data-testid="fruit-option">
+                        Strawberries
                     </option>
                 </select>
                 &nbsp;&nbsp;
                 <label htmlFor="fruit">Choose a variety: </label>
                 <select name="fruit" className="fruit-select" id="fruit" data-testid="fruit-select">
                     <option value="one" data-testid="fruit-option">
-                        Cavendish
+                        One
                     </option>
                     <option value="two" data-testid="fruit-option">
                         Two
